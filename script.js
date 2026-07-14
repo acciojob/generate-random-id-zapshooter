fc123dd371866849ba56f24b03780e3f77fcd87f
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-07-14 23:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function makeid(l) {
-  // write your code here
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < l; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
 }
-
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
